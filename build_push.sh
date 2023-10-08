f462b7007ae8f3075eb0d3d45ef0a9ddfab81351
Samuel van Eck <samuel.vaneck@mobiliteitsfabriek.nl>
2023-10-08 17:46:10 +0200

    Update Alpine image

--- build_push.sh
+++ build_push.sh
@@ -1,4 +1,5 @@
+set -xe
+
 docker build -t ghcr.io/samuelvaneck/ruby_ocr:latest .
 echo $CR_PAT | docker login ghcr.io --username samuelvaneck --password-stdin
-docker tag ghcr.io/samuelvaneck/ruby_ocr:latest
 docker push ghcr.io/samuelvaneck/ruby_ocr:latest
